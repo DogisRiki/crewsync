aaf26c1a02c2a905ea52abc9ce014cac082e25dc
Riki <dogisriki1122@gmail.com>
2023-07-16 03:02:17 +0900

    一旦ログイン画面のViewの部分のみ

--- api/crewsync/src/main/java/com/example/crewsync/RootController.java
+++ api/crewsync/src/main/java/com/example/crewsync/RootController.java
@@ -1,6 +1,7 @@
 package com.example.crewsync;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class RootController {
     @RequestMapping("/")
     public String root() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
--- api/crewsync/src/main/java/com/example/crewsync/conf/SecurityConfig.java
+++ api/crewsync/src/main/java/com/example/crewsync/conf/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.crewsync.conf;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     /**
@@ -25,7 +28,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(login -> login
-                .permitAll()).authorizeHttpRequests(auth -> auth
+                .loginPage("/login").permitAll())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers("/img/**").permitAll()
+                        .antMatchers("/").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
